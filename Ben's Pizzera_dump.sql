618d75271f21e954a7f3c8f844b2795b2739e44b
SaikiaP <130836171+SaikiaP@users.noreply.github.com>
2023-12-12 00:07:26 +0530

    Update Ben's Pizzera_dump.sql

--- Ben's Pizzera_dump.sql
+++ Ben's Pizzera_dump.sql
@@ -1,9 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE `Orders` (
     `row_id` int  NOT NULL ,
